50bc46e4582bb930e89eaf2b30dd785ebc94a2ef
Juan Pablo Urrea Correa <juan10268@hotmail.com>
2018-07-23 15:22:51 -0500

    Modelo Persona

---
+++ Cuidar/Controllers/PersonaController.cs
@@ -0,0 +1,21 @@
+﻿using Cuidar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Cuidar.Controllers
+{
+    public class PersonaController : ApiController
+    {
+        [Route("/api/Persona/Agregar")]
+        [HttpPost]
+        [AllowAnonymous]
+        public string Registrar(Persona persona)
+        {
+            Console.Write("Llego");
+        }
+    }
+}
---
+++ Cuidar/Models/Persona.cs
@@ -0,0 +1,16 @@
+﻿namespace Cuidar.Models
+{
+    public class Persona
+    {
+        public int personaID { get; set; }
+        public string personaNombre { get; set; }
+        public string personaApellido1 { get; set; }
+        public string personaApellido2 { get; set; }
+        public string personaNacimiento { get; set; }
+        public string personaTelefono { get; set; }
+        public string personaEstadoCivil { get; set; }
+        public string personaGenero { get; set; }
+        public int personaCiudad { get; set; }
+        public int personaTipoDocumento { get; set; }
+    }
+}
