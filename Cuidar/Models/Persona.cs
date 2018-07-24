d1998ddbfcd37879ff425d0e20bc0b802fff1035
Juan Pablo Urrea Correa <juan10268@hotmail.com>
2018-07-24 11:30:59 -0500

    Llegada a base de datos.

--- Cuidar/Models/Persona.cs
+++ Cuidar/Models/Persona.cs
@@ -6,8 +6,9 @@
         public string personaNombre { get; set; }
         public string personaApellido1 { get; set; }
         public string personaApellido2 { get; set; }
-        public string personaNacimiento { get; set; }
+        public string personaFechaNacimiento { get; set; }
         public string personaTelefono { get; set; }
+        public string personaDireccion { get; set; }
         public string personaEstadoCivil { get; set; }
         public string personaGenero { get; set; }
         public int personaCiudad { get; set; }
