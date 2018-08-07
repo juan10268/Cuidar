85b3407e00d8c57fe9b7d6de40739f1c0601f25a
Juan Pablo Urrea Correa <juan10268@hotmail.com>
2018-08-06 19:53:08 -0500

    Creacion de historia general

---
+++ Cuidar/Models/HistoriaGeneral.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cuidar.Models
+{
+    public class HistoriaGeneral
+    {
+        public int CitaID { get; set; }
+        public int HistoriaClinicaID { get; set; }
+    }
+}
---
+++ Cuidar/Models/TipoAntecedentes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cuidar.Models
+{
+    public class TipoAntecedentes
+    {
+        public int TipoAntecedentesID { get; set; }
+        public string TipoAntecedentesNombre { get; set; }
+    }
+}
